9c6d29d16a682463faac93a6f7c6eedee60c493c
Merjen Charyyeva <merjen.chary@gmail.com>
2019-01-14 21:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function destructivelyAppendKitten(name){
  return kittens
 }
 //Arrays destructivelyPrependKitten(name) prepends a kitten to the beginningof the kittens array:
+function destructivelyPrependKitten(name){
+ kittens.unshift(name)
+ return kittens;
+}
