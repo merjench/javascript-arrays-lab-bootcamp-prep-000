43ed7c9ce27ee88a27ca1db9b1489fd026543f42
Merjen Charyyeva <merjen.chary@gmail.com>
2019-01-14 21:28:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ function appendKitten(name){
 // Arrays prependKitten(name) prepends a kitten to the kittens array and returns a new array, leaving the kittens array unchanged:
 
 //Arrays removeLastKitten() removes the last kitten in the kittens array andreturns a new array, leaving the kittens array unchanged:
+
+//Arrays removeFirstKitten() removes the first kitten from the kittens arrayand returns a new array, leaving the kittens array unchanged:
+
+
+
